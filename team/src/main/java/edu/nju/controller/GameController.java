69fccf4ed807683a44f14231c88bd0633e89bbd3
lycheeshell <lycheeshell@163.com>
2020-01-08 23:05:47 +0800

    littie modify

--- team/src/main/java/edu/nju/controller/GameController.java
+++ team/src/main/java/edu/nju/controller/GameController.java
@@ -1,8 +1,13 @@
 package edu.nju.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @Author ：lycheeshell
  * @Date ：Created in 16:42 2020/1/5
  */
+@RestController
+@RequestMapping("/team/game")
 public class GameController {
 }
--- team/src/main/java/edu/nju/controller/PlayController.java
+++ team/src/main/java/edu/nju/controller/PlayController.java
@@ -1,8 +1,13 @@
 package edu.nju.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @Author ：lycheeshell
  * @Date ：Created in 16:43 2020/1/5
  */
+@RestController
+@RequestMapping("/team/play")
 public class PlayController {
 }
